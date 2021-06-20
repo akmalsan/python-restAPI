82b0287a5ee693b8f8e16ae3f261a4468cf35f3d
ihsan <akmalssan@gmail.com>
2021-06-20 15:51:42 +0700

    create object 'name'

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ app = Flask(__name__)
 # Wrap the app in an API
 api = Api(app)
 
+names = {"tom": {"age": 19, "gender": "male"},
+         "dee": {"age": 19, "gender": "female"}}
+
 
 class HelloWorld(Resource):
     # Override the GET method
-    def get(self, name, test):
-        return {"name": name, "test": test}
+    def get(self, name):
+        return names[name]
 
     def post(self):
         return {"data": "posted"}
 
 
 # Define the Hello World endpoint
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     # Run app in debug mode, see all the output and debug information
