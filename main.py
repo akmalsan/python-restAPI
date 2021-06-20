b32023fb68d5141c124c510c6a693c6ee3e6cb1a
ihsan <akmalssan@gmail.com>
2021-06-20 15:44:36 +0700

    added parameter to endpoint

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ api = Api(app)
 
 class HelloWorld(Resource):
     # Override the GET method
-    def get(self):
-        return {"data": "Hello World"}
+    def get(self, name, test):
+        return {"name": name, "test": test}
 
     def post(self):
         return {"data": "posted"}
 
 
 # Define the Hello World endpoint
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
 
 if __name__ == "__main__":
     # Run app in debug mode, see all the output and debug information
