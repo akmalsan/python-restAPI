570649454b234cb1d94832eb9c674c76ce725972
ihsan <akmalssan@gmail.com>
2021-06-20 16:06:52 +0700

    create args and request parser

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 # Create Flask app
 app = Flask(__name__)
 # Wrap the app in an API
 api = Api(app)
+
+# create request parser object to parse through requests, make sure it follows the args guidelines
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video")
+video_put_args.add_argument("views", type=int, help="Views of the video")
+video_put_args.add_argument("likes", type=int, help="Likes of the video")
 
 videos = {}
 
@@ -14,6 +20,8 @@ class Video(Resource):
         return videos[video_id]
 
     def put(self, video_id):
+        # get all the args from video_put_args, if not sent automatically send back error message
+        args = video_put_args.parse_args()
         return
 
 
