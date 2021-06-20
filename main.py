0a3dedb423a248c44530ba20786813c835b4c420
ihsan <akmalssan@gmail.com>
2021-06-20 15:27:47 +0700

    create helloworl class

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 # Wrap the app in an API
 api = Api(app)
 
+# Create a class that is a Resource, override several methods from Resource
+
+
+class HelloWorld(Resource):
+    # Override the GET method
+    def get(self):
+        return {"Hello World"}
+
+
+# Define the Hello World endpoint
+api.add_resource(HelloWorld, "/helloworld")
+
 if __name__ == "__main__":
     # Run app in debug mode, see all the output and debug information
     app.run(debug=True)
