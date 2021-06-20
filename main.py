bc09a42fba0f3c32685e3b8dc9700eac4c8e820b
ihsan <akmalssan@gmail.com>
2021-06-20 16:31:40 +0700

    added abort if exist and delete method

--- main.py
+++ main.py
@@ -18,23 +18,35 @@ video_put_args.add_argument(
 videos = {}
 
 
-def abort_if_video_id_null(video_id):
+def abort_if_video_id_doesnt_exist(video_id):
     # abort if video doesnt exist
     if video_id not in videos:
-        abort(404, message="Video id is not valid")
+        abort(404, message="Could not find video with that ID")
+
+
+def abort_if_video_exists(video_id):
+    # abort if video already exists
+    if video_id in videos:
+        abort(409, message="Video already exists with that ID")
 
 
 class Video(Resource):
     def get(self, video_id):
-        abort_if_video_id_null(video_id)
+        abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_exists(video_id)
         # get all the args from video_put_args, if not sent automatically send back error message
         args = video_put_args.parse_args()
         videos[video_id] = args  # add videos
         # 201 is a status code, stands for data created
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
 
 # Define the Hello World endpoint
